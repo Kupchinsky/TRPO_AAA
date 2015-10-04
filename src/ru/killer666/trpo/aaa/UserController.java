9eab5630c8e2288632906c2fb1915eec0ca168c3
Kupchinsky <user@killer666.ru>
2015-10-04 19:26:08 +0700

    * Added mysql-connector-java-5.1.36.jar into classpath * Сделан метод входа

--- src/ru/killer666/trpo/aaa/UserController.java
+++ src/ru/killer666/trpo/aaa/UserController.java
@@ -3,10 +3,12 @@ package ru.killer666.trpo.aaa;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.commons.codec.digest.DigestUtils;
-import ru.killer666.trpo.aaa.Models.Accounting;
-import ru.killer666.trpo.aaa.Models.Resource;
-import ru.killer666.trpo.aaa.Models.User;
+import ru.killer666.trpo.aaa.models.Accounting;
+import ru.killer666.trpo.aaa.models.Resource;
+import ru.killer666.trpo.aaa.models.Role;
+import ru.killer666.trpo.aaa.models.User;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,41 +16,109 @@ import java.sql.SQLException;
 public class UserController {
     @Getter
     private User logOnUser = null;
-    @Getter
-    private Resource resource = null;
+
     @Getter
     @Setter
     private Accounting userAccounting = null;
     private static Database db = new Database(DatabaseConfig.host, DatabaseConfig.port, DatabaseConfig.database, DatabaseConfig.userName, DatabaseConfig.password);
 
-    private boolean isValidPassword(String password, String passwordHash, String salt) {
-        return DigestUtils.sha1Hex(password + salt).equals(passwordHash);
+    private String encryptPassword(String password, String salt) {
+        return DigestUtils.sha1Hex(DigestUtils.sha1Hex(password) + salt);
     }
 
-    public void logIn(String userName, String password, String resourceName) throws UserNotFoundException, IncorrectPasswordException, ResourceDeniedException {
-        PreparedStatement preparedStatement = null;
+    public void logIn(String userName, String password, String resourceName, int role) throws UserNotFoundException, IncorrectPasswordException, ResourceDeniedException, ResourceNotFoundException {
+        Connection connection = null;
 
         try {
-            preparedStatement = UserController.db.getConnection().prepareStatement("SELECT * FROM `users` WHERE `login`=? LIMIT 1");
+            connection = UserController.db.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM `users` WHERE `login`=?");
             preparedStatement.setString(1, userName);
-            ResultSet result = preparedStatement.executeQuery();
+            ResultSet resultUser = preparedStatement.executeQuery();
 
-            if (!result.next())
+            if (!resultUser.first()) {
                 throw new UserNotFoundException();
+            }
 
-            String passwordHash = result.getString("passwordHash");
-            String salt = result.getString("salt");
-
-            if (!this.isValidPassword(password, passwordHash, salt))
+            // Checking password
+            if (this.encryptPassword(password, resultUser.getString("salt")).equals(resultUser.getString("passwordHash"))) {
                 throw new IncorrectPasswordException();
+            }
 
-            // TODO: Check resource access
+            // Checking resource exists
+            preparedStatement = UserController.db.getConnection().prepareStatement("SELECT * FROM `resources` WHERE `name`=?");
+            preparedStatement.setString(1, resourceName);
+            ResultSet resultResource = preparedStatement.executeQuery();
+
+            if (!resultResource.first()) {
+                throw new ResourceNotFoundException();
+            }
+
+            // Checking resource access
+            boolean accessGranted = false;
+            int lastResourceId = resultResource.getInt("id");
+            int lastParentResourceId = resultResource.getInt("parent_resource_id");
+            boolean hasParentResourceId = !resultResource.wasNull();
+
+            while (true) {
+                // Checking access for this resource
+                preparedStatement = UserController.db.getConnection().prepareStatement("SELECT * FROM `resources_users` WHERE `resource_id`=? AND `user_id`=?");
+                preparedStatement.setInt(1, lastResourceId);
+                preparedStatement.setInt(2, resultUser.getInt("id"));
+                ResultSet resultAccess = preparedStatement.executeQuery();
+
+                if (resultAccess.first()) {
+                    accessGranted = true;
+                    break;
+                }
+
+                // Finding parent resources with access
+                if (!hasParentResourceId) {
+                    break;
+                }
+
+                preparedStatement = UserController.db.getConnection().prepareStatement("SELECT * FROM `resources` WHERE `id`=?");
+                preparedStatement.setInt(1, lastParentResourceId);
+
+                ResultSet resultParentResource = preparedStatement.executeQuery();
+                lastResourceId = resultParentResource.getInt("id");
+                lastParentResourceId = resultParentResource.getInt("parent_resource_id");
+                hasParentResourceId = !resultParentResource.wasNull();
+            }
+
+            if (!accessGranted) {
+                throw new ResourceDeniedException();
+            }
+
+            this.logOnUser = new User(resultUser.getInt("id"), resultUser.getString("login"), resultUser.getString("passwordHash"), resultUser.getString("salt"), resultUser.getString("personName"));
+            this.userAccounting = new Accounting(this.logOnUser, Role.fromInt(role));
+
+            // Finding all child resources
+            Resource lastParentResource = new Resource(resultResource.getInt("id"), resultResource.getString("name"));
+            this.userAccounting.getResources().add(lastParentResource);
+
+            while (true) {
+                preparedStatement = UserController.db.getConnection().prepareStatement("SELECT * FROM `resources` WHERE `parent_resource_id`=?");
+                preparedStatement.setInt(1, lastParentResource.getDatabaseId());
+
+                ResultSet resultChildResource = preparedStatement.executeQuery();
+
+                if (!resultChildResource.first()) {
+                    break;
+                }
+
+                Resource newResource = new Resource(resultChildResource.getInt("id"), resultChildResource.getString("name"));
+                newResource.setParentResource(lastParentResource);
+                this.userAccounting.getResources().add(newResource);
+
+                lastParentResource = newResource;
+            }
         } catch (SQLException e) {
+            // Error while working
+            e.printStackTrace();
         } finally {
-            if (preparedStatement != null) {
+            if (connection != null) {
                 try {
-                    preparedStatement.close();
-                    preparedStatement.getConnection().close();
+                    connection.close();
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
@@ -67,5 +137,8 @@ public class UserController {
     }
 
     public static class ResourceDeniedException extends Exception {
+    }
+
+    public static class ResourceNotFoundException extends Exception {
     }
 }
