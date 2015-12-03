c27c9e72193136f6107d95f0d70fd734acd48b4c
Kupchinsky <user@killer666.ru>
2015-12-03 09:17:28 +0700

    - Updated

--- src/main/java/ru/killer666/trpo/aaa/services/AuthService.java
+++ src/main/java/ru/killer666/trpo/aaa/services/AuthService.java
@@ -1,11 +1,13 @@
 package ru.killer666.trpo.aaa.services;
 
 import lombok.Getter;
+import lombok.NonNull;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import ru.killer666.trpo.aaa.RoleInterface;
 import ru.killer666.trpo.aaa.domains.*;
 import ru.killer666.trpo.aaa.exceptions.IncorrectPasswordException;
@@ -42,45 +44,35 @@ public class AuthService {
         return DigestUtils.shaHex(DigestUtils.shaHex(password) + salt);
     }
 
-    public List<Resource> getAllResources() {
+    @SuppressWarnings("unused")
+    public List<Resource> getAllResources(@NonNull Session session) {
         AuthService.logger.debug("Fetching all resources");
-
-        Session session = this.hibernateService.getSession();
 
         @SuppressWarnings("unchecked")
         List<Resource> resources = session.createCriteria(Resource.class).list();
-        session.close();
 
         return resources;
     }
 
-    public Resource getResourceByName(String resourceName) throws ResourceNotFoundException {
+    public Resource getResourceByName(@NonNull Session session, @NonNull String resourceName) throws ResourceNotFoundException {
         AuthService.logger.debug("Fetching resource " + resourceName);
-
-        Session session = this.hibernateService.getSession();
 
         Query query = session.createQuery("from Resource where name = :name");
         query.setString("name", resourceName);
 
         List result = query.list();
 
-        try {
-            if (result.size() == 0) {
-                throw new ResourceNotFoundException(resourceName);
-            }
-
-            return (Resource) result.iterator().next();
-        } finally {
-            session.close();
+        if (result.size() == 0) {
+            throw new ResourceNotFoundException(resourceName);
         }
+
+        return (Resource) result.iterator().next();
     }
 
     /* Для внешнего использования */
     @SuppressWarnings("unused")
-    public List<RoleInterface> getGrantedRoles(Resource resource) throws SQLException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public List<RoleInterface> getGrantedRoles(@NonNull Session session, @NonNull Resource resource) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         AuthService.logger.debug("Getting roles for resource");
-
-        Session session = this.hibernateService.getSession();
 
         Query query = session.createQuery("from ResourceWithRole where resource_id = :resource_id AND user_id = :user_id");
         query.setInteger("resource_id", resource.getDatabaseId());
@@ -88,7 +80,6 @@ public class AuthService {
 
         @SuppressWarnings("unchecked")
         List<ResourceWithRole> queryResult = query.list();
-        session.close();
 
         Method values = this.roleInterfaceClass.getDeclaredMethod("values");
         Object[] enumConstants = (Object[]) values.invoke(null);
@@ -102,58 +93,46 @@ public class AuthService {
         return result;
     }
 
-    public void authResource(Resource resource, RoleInterface role) throws ResourceNotFoundException, ResourceDeniedException, SQLException {
+    public void authResource(@NonNull Session session, @NonNull Resource resource, @NonNull RoleInterface role) throws ResourceNotFoundException, ResourceDeniedException {
         AuthService.logger.debug("Authorizing resource");
-
-        Session session = this.hibernateService.getSession();
 
         Query query = session.createQuery("from ResourceWithRole where resource_id = :resource_id AND user_id = :user_id AND role = :role");
         query.setInteger("resource_id", resource.getDatabaseId());
         query.setInteger("user_id", this.logOnUser.getDatabaseId());
         query.setInteger("role", role.ordinal());
 
-        try {
-            List result = query.list();
+        List result = query.list();
 
-            if (result.size() == 0) {
-                throw new ResourceDeniedException(resource.getName(), this.logOnUser.getLogin());
-            }
-
-            ResourceWithRole resourceWithRole = (ResourceWithRole) result.iterator().next();
-
-            AccountingResource accountingResource = new AccountingResource();
-            accountingResource.setAccounting(this.logOnUserAccounting);
-            accountingResource.setResourceWithRole(resourceWithRole);
-
-            this.logOnUserAccounting.getResources().add(accountingResource);
-        } finally {
-            session.close();
+        if (result.size() == 0) {
+            throw new ResourceDeniedException(resource.getName(), this.logOnUser.getLogin());
         }
+
+        ResourceWithRole resourceWithRole = (ResourceWithRole) result.iterator().next();
+
+        AccountingResource accountingResource = new AccountingResource();
+        accountingResource.setAccounting(this.logOnUserAccounting);
+        accountingResource.setResourceWithRole(resourceWithRole);
+
+        this.logOnUserAccounting.getResources().add(accountingResource);
 
         AuthService.logger.debug("Authorized on resource " + resource.getName());
     }
 
-    public void authUser(String userName, String password) throws UserNotFoundException, IncorrectPasswordException, SQLException {
+    public void authUser(@NonNull Session session, @NonNull String userName, @NonNull String password) throws UserNotFoundException, IncorrectPasswordException {
         AuthService.logger.debug("Authorizing user");
-
-        Session session = this.hibernateService.getSession();
 
         Query query = session.createQuery("from User where login = :login");
         query.setString("login", userName);
 
         User user;
 
-        try {
-            List result = query.list();
+        List result = query.list();
 
-            if (result.size() == 0) {
-                throw new UserNotFoundException(userName);
-            }
-
-            user = (User) result.iterator().next();
-        } finally {
-            session.close();
+        if (result.size() == 0) {
+            throw new UserNotFoundException(userName);
         }
+
+        user = (User) result.iterator().next();
 
         // Checking password
         if (!this.encryptPassword(password, user.getSalt()).equals(user.getPasswordHash())) {
@@ -167,7 +146,7 @@ public class AuthService {
         AuthService.logger.debug("Authorized as user " + this.logOnUser.getLogin() + " (" + this.logOnUser.getPersonName() + ")");
     }
 
-    public void saveAccounting() throws SQLException {
+    public void saveAccounting(@NonNull Session session) throws SQLException {
         if (this.logOnUserAccounting.getLogoutDate() == null) {
             this.logOnUserAccounting.setLogoutDate(Calendar.getInstance().getTime());
             AuthService.logger.debug("Logout date == null, setting up to current time");
@@ -175,15 +154,12 @@ public class AuthService {
 
         AuthService.logger.debug("Saving accounting");
 
-        Session session = this.hibernateService.getSession();
-
-        session.beginTransaction();
+        Transaction tx = session.beginTransaction();
 
         session.save(this.logOnUserAccounting);
         this.logOnUserAccounting.getResources().forEach(session::save);
 
-        session.getTransaction().commit();
-        session.close();
+        tx.commit();
 
         this.clearAll();
     }
